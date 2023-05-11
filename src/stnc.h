3d590f638a2d63f1aa866e12a8befc8935465284
Lior Vinman <liorvi35@gmail.com>
2023-05-11 12:13:40 +0300

    Update stnc.h

--- src/stnc.h
+++ src/stnc.h
@@ -7,6 +7,8 @@
 
 #ifndef _STNC_H_
 
+    #include <stdio.h>
+
     /**
      * @brief a flag to mark the header file (so wont be included more than once in total)
      */
@@ -33,6 +35,11 @@
     #define TIME_LENGTH 25
 
     /**
+     * @brief 100MB for generating a chunk of data
+    */
+    #define MB_100 100 * 1024 * 1024
+
+    /**
      * @brief client's socket file descriptor
      */
     int client_sock = 0;
@@ -41,6 +48,11 @@
      * @brief listening socket file descriptor for server
      */
     int server_sock = 0;
+
+    /**
+     * @brief a buffer with random 100MB
+    */
+    char *chunk = NULL;
 
     /**
      * @brief this function is the client chat functionality
@@ -60,5 +72,27 @@
      * @param int signal ID
     */
     void exit_handle(int);
+
+    /**
+     * @brief this function is a performace test for client
+     * @param char* an ipv4 address of the server
+     * @param char* a port of the server
+     * @param char* type of communication: ipv4, ipv6, uds, mmap, pipe
+     * @param char* type parameter: tcp, udp, stream, dgram, filename
+    */
+    void client_performace(char*, char*, char*, char*);
+
+    /**
+     * @brief this function is a performace test for server
+     * @param char* a port of the server
+    */
+    void server_performace(char*);
+
+    /**
+     * @brief this function is for generating random 100MB
+     * @param char* a pointer to buffer
+     * @param size_t how much data generate
+    */
+    void generate_data(char*, size_t);
 
 #endif
