08e0c56414cb00729a859977a091932be526eea8
Lior Vinman <liorvi35@gmail.com>
2023-05-15 09:59:12 +0300

    Update testServer.c

--- src/testServer.c
+++ src/testServer.c
@@ -15,15 +15,35 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/un.h>
 
 #define SO_REUSEPORT 15
 #define MB_100 1024 * 1024 * 100
+
+unsigned int hash_xor(const char *buffer, size_t len) {
+  unsigned int hash = 0;
+  for (size_t i = 0; i < len; i++) {
+    hash ^= buffer[i];
+  }
+  return hash;
+}
+
+unsigned int hash_file(FILE *fp)
+{
+    unsigned int hash = 5381;
+    char ch;
+    while (fread(&ch, 1, 1, fp) == 1)
+    {
+        hash = (hash * 33) + ch;
+    }
+    return hash;
+}
 
 void performance_server(char *port, bool qFlag)
 {
     int server_sock = 0, client_sock = 0, optval = 0;
 
-    socklen_t addr_size = 0, addr_size6 = 0;
+    socklen_t addr_size = 0, addr_size6 = 0, addr_size_unix = 0;
     struct sockaddr_in server_addr = {0}, client_addr = {0};
     memset(&server_sock, 0, sizeof(server_addr));
     memset(&client_sock, 0, sizeof(client_sock));
@@ -32,8 +52,6 @@ void performance_server(char *port, bool qFlag)
     memset(type, 0, sizeof(type));
     memset(type, 0, sizeof(param));
     memset(type, 0, sizeof(buffer));
-
-    size_t size = 0;
 
     ssize_t bytes_recv = 0;
 
@@ -49,6 +67,9 @@ void performance_server(char *port, bool qFlag)
     memset(&stat, 0, sizeof(struct stat));
 
     void *ptr = NULL;
+
+    struct sockaddr_un unix_addr = {0};
+    memset(&unix_addr, 0, sizeof(struct sockaddr_un));
 
     server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (server_sock <= 0)
@@ -338,7 +359,6 @@ void performance_server(char *port, bool qFlag)
         server_addr6.sin6_addr = in6addr_any;
         server_addr6.sin6_port = htons(atoi(port));
 
-
         if (bind(server_sock, (struct sockaddr *)&server_addr6, addr_size6) < 0)
         {
             perror("bind() failed");
@@ -366,32 +386,149 @@ void performance_server(char *port, bool qFlag)
 
         end.tv_sec -= 1;
     }
-    else if(!strcmp(type, "uds") && !strcmp(param, "stream"))
+    else if (!strcmp(type, "uds") && !strcmp(param, "stream"))
     {
+        server_sock = socket(AF_UNIX, SOCK_STREAM, PF_UNIX);
+        if (server_sock <= 0)
+        {
+            perror("socket() failed");
+            exit(errno);
+        }
 
+        unlink("/tmp/uds_socket");
+
+        addr_size_unix = sizeof(struct sockaddr_un);
+        unix_addr.sun_family = AF_UNIX;
+
+        strncpy(unix_addr.sun_path, "/tmp/uds_socket", addr_size_unix - 1);
+
+        if (bind(server_sock, (struct sockaddr *)&unix_addr, addr_size_unix) < 0)
+        {
+            perror("bind() failed");
+            close(server_sock);
+            exit(errno);
+        }
+
+        if (listen(server_sock, 1) < 0)
+        {
+            perror("listen() failed");
+            close(server_sock);
+            exit(errno);
+        }
+
+        client_sock = accept(server_sock, NULL, NULL);
+        if (client_sock <= 0)
+        {
+            perror("listen() failed");
+            close(server_sock);
+            exit(errno);
+        }
+
+        gettimeofday(&start, NULL);
+        bytes_recv = recv(client_sock, buffer, sizeof(buffer), 0);
+        while (bytes_recv > 0)
+        {
+            bytes_recv = recv(client_sock, buffer, sizeof(buffer), 0);
+        }
+        if (bytes_recv < 0)
+        {
+            perror("recv() failed");
+            close(client_sock);
+            close(server_sock);
+            exit(errno);
+        }
+        gettimeofday(&end, NULL);
+
+        if (shutdown(client_sock, SHUT_RDWR) < 0)
+        {
+            perror("shutdown() failed");
+            close(client_sock);
+            close(server_sock);
+            exit(errno);
+        }
+        close(client_sock);
+        close(server_sock);
     }
-    else if(!strcmp(type, "uds") && !strcmp(param, "dgram"))
+    else if (!strcmp(type, "uds") && !strcmp(param, "dgram"))
     {
-        
+        server_sock = socket(AF_UNIX, SOCK_DGRAM, PF_UNIX);
+        if (server_sock <= 0)
+        {
+            perror("socket() failed");
+            exit(errno);
+        }
+
+        unlink("/tmp/uds_socket");
+
+        addr_size_unix = sizeof(struct sockaddr_un);
+        unix_addr.sun_family = AF_UNIX;
+
+        strncpy(unix_addr.sun_path, "/tmp/uds_socket", addr_size_unix - 1);
+
+        if (bind(server_sock, (struct sockaddr *)&unix_addr, addr_size_unix) < 0)
+        {
+            perror("bind() failed");
+            close(server_sock);
+            exit(errno);
+        }
+
+        gettimeofday(&start, NULL);
+        bytes_recv = recvfrom(server_sock, buffer, sizeof(buffer), 0, NULL, NULL);
+        while (strcmp(buffer, "END"))
+        {
+            memset(buffer, 0, sizeof(buffer));
+            bytes_recv = recvfrom(server_sock, buffer, sizeof(buffer), 0, NULL, NULL);
+        }
+        if (bytes_recv < 0)
+        {
+            perror("recv() failed");
+            close(client_sock);
+            close(server_sock);
+            exit(errno);
+        }
+        gettimeofday(&end, NULL);
+
+        close(server_sock);
+
+        end.tv_sec -= 1;
     }
-    else if(!strcmp(type, "pipe"))
+    else if (!strcmp(type, "pipe"))
     {
-        
-    }
-    else if(!strcmp(type, "mmap"))
-    {
-        sleep(2);
-        server_sock = open("shared_memory", O_RDWR | O_CREAT, 0666);
-        if(server_sock <= 0)
+        mkfifo("/tmp/pipe", 0666);
+
+        server_sock = open("/tmp/pipe", O_RDONLY);
+        if (server_sock <= 0)
         {
             perror("open() failed");
             exit(errno);
         }
 
-        fstat(server_sock, &stat);
-        size = stat.st_size;
+        gettimeofday(&start, NULL);
+        bytes_recv = read(server_sock, buffer, BUFSIZ);
+        while (bytes_recv > 0)
+        {
+            bytes_recv = read(server_sock, buffer, BUFSIZ);
+        }
 
-        ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, server_sock, 0);
+        gettimeofday(&end, NULL);
+    }
+    else if (!strcmp(type, "mmap"))
+    {
+        server_sock = open(param, O_RDWR | O_CREAT, 0666);
+        if (server_sock <= 0)
+        {
+            perror("open() failed");
+            exit(errno);
+        }
+
+        if (ftruncate(server_sock, MB_100) < 0)
+        {
+            perror("ftruncate() error");
+            close(server_sock);
+            exit(errno);
+        }
+
+        ptr = mmap(NULL, MB_100, PROT_READ | PROT_WRITE, MAP_SHARED, server_sock, 0);
         if (ptr == MAP_FAILED)
         {
             perror("mmap() failed");
@@ -399,11 +536,50 @@ void performance_server(char *port, bool qFlag)
             exit(errno);
         }
 
+        client_sock = open("mmap.bin", O_WRONLY | O_CREAT, 0666);
+        if (client_sock <= 0)
+        {
+            perror("open() failed");
+            close(server_sock);
+            exit(errno);
+        }
+
         gettimeofday(&start, NULL);
-        memcpy(ptr, buffer, strlen(buffer) + 1);
+        if (write(client_sock, ptr, MB_100) < 0)
+        {
+            perror("write() failed");
+            close(server_sock);
+            exit(errno);
+        }
         gettimeofday(&end, NULL);
 
+        if (munmap(ptr, MB_100) < 0)
+        {
+            perror("munmap() failed");
+            close(server_sock);
+            exit(errno);
+        }
+
+        close(client_sock);
         close(server_sock);
     }
+
+    if (!strcmp(type, "mmap") || !strcmp(type, "pipe"))
+    {
+        fprintf(stdout, "%s,%ld\n", type, ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+
+        if (!qFlag)
+        {
+            fprintf(stdout, "Checksum=%ld\n", hash(buffer));
+        }
+
+        return;
+    }
+
     fprintf(stdout, "%s_%s,%ld\n", type, param, ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+
+    if (!qFlag)
+    {
+            fprintf(stdout, "Checksum=%ld\n", hash(buffer));
+    }
 }
